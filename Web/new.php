d04e4eea1cae351cfe3d4f0ba7a70500e21d9aa8
EduardoWhite <47611990+EduardoWhite@users.noreply.github.com>
2020-05-09 05:57:18 -0700

    Libreria Mensaje
    Cree una libreria para mostrar el mensaje de validación.

--- Web/new.php
+++ Web/new.php
@@ -25,6 +25,13 @@
     <!-- MIS SCRIPT -->
     <script src="js/new-user.js"></script>
     <link rel="stylesheet" href="css/new.css">
+    <!-- ESTA LIBRERIA LA CREO: EDUARDO BLANCO
+        PARA PODER MANDAR A LLAMAR UN MENSAJE
+        DE VALIDACION
+    -->
+    <script src="js/msg-alert.js"></script>
+    <link rel="stylesheet" href="css/msg-alert.css">
+    <!-- FIN DE LA LIBRERIA -->
     <title></title>
   </head>
   <body>
@@ -38,39 +45,87 @@
             <label for="">Nombre</label>
             <img src="img/name.png" alt="" class="icon">
             <input type="text" placeholder="Nombre" class="textbox" name="usuario" required autocomplete="off">
+
+            <!-- ELEMENTOS Y CLASES PARA USAR LIBREARIA CREADA POR EDUARDO BLANCO -->
+            <div class="container-msg">
+              <p class="title-msg">Titulo de mensaje 1</p>
+              <p class="title-content-msg">Mensaje 1</p>
+            </div>
+            <!-- FIN -->
           </div>
           <div class="field line">
             <label for="">Apellidos</label>
             <img src="img/name.png" alt="" class="icon">
             <input type="text" placeholder="Apellidos" class="textbox" name="apellido" required autocomplete="off">
+
+            <!-- ELEMENTOS Y CLASES PARA USAR LIBREARIA CREADA POR EDUARDO BLANCO -->
+            <div class="container-msg">
+              <p class="title-msg">Titulo de mensaje 2</p>
+              <p class="title-content-msg">Mensaje 2</p>
+            </div>
+            <!-- FIN -->
           </div>
           <div class="field line">
             <label for="">Correo</label>
             <img src="img/mail.png" alt="" class="icon">
             <input type="text" placeholder="Correo" class="textbox" name="correo1" required autocomplete="off">
+
+            <!-- ELEMENTOS Y CLASES PARA USAR LIBREARIA CREADA POR EDUARDO BLANCO -->
+            <div class="container-msg">
+              <p class="title-msg">Titulo de mensaje 3</p>
+              <p class="title-content-msg">Mensaje 3</p>
+            </div>
+            <!-- FIN -->
           </div>
           <div class="field line">
             <label for="">Confirmar</label>
             <img src="img/mail.png" alt="" class="icon">
             <input type="text" placeholder="Confirmar" class="textbox" name="correo2" required autocomplete="off">
+
+            <!-- ELEMENTOS Y CLASES PARA USAR LIBREARIA CREADA POR EDUARDO BLANCO -->
+            <div class="container-msg">
+              <p class="title-msg">Titulo de mensaje 4</p>
+              <p class="title-content-msg">Mensaje 4</p>
+            </div>
+            <!-- FIN -->
           </div>
           <div class="field line">
             <label for="">Contraseña</label>
             <img src="img/lock.png" alt="" class="icon">
             <input type="password" placeholder="Contraseña" class="textbox" name="contrasena1" required autocomplete="off">
+
+            <!-- ELEMENTOS Y CLASES PARA USAR LIBREARIA CREADA POR EDUARDO BLANCO -->
+            <div class="container-msg">
+              <p class="title-msg">Titulo de mensaje 5</p>
+              <p class="title-content-msg">Mensaje 5</p>
+            </div>
+            <!-- FIN -->
           </div>
           <div class="field line">
             <label for="">Confirmar</label>
             <img src="img/lock.png" alt="" class="icon">
             <input type="password" placeholder="Confirmar" class="textbox" name="contrasena2" required autocomplete="off">
+
+            <!-- ELEMENTOS Y CLASES PARA USAR LIBREARIA CREADA POR EDUARDO BLANCO -->
+            <div class="container-msg">
+              <p class="title-msg">Titulo de mensaje 6</p>
+              <p class="title-content-msg">Mensaje 6</p>
+            </div>
+            <!-- FIN -->
           </div>
           <div class="field line" style=""> <!-- Telefono -->
             <label for="">Teléfono</label>
             <img src="img/name.png" alt="" class="icon">
             <input type="text" class="textbox" name="telefono" id="txt-tel">
+
+            <!-- ELEMENTOS Y CLASES PARA USAR LIBREARIA CREADA POR EDUARDO BLANCO -->
+            <div class="container-msg">
+              <p class="title-msg">Titulo de mensaje 7</p>
+              <p class="title-content-msg">Mensaje 7</p>
+            </div>
+            <!-- FIN -->
           </div>
           <div class="field" id="field-button">
-            <p class="msg">* Datos incorrectos</p>
             <div class="container-buttons">
               <input type="button" class="button-cancel" value="Cancel">
               <input type="submit" name=""  class="button-save" value="Guardar">
--- Web/pruebaIgnorar.php
+++ Web/pruebaIgnorar.php
@@ -2,37 +2,31 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <!-- JQUERY -->
+    <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+    <script src="js/msg-alert.js"></script>
+    <link rel="stylesheet" href="css/msg-alert.css">
     <title></title>
+    <style>
+    body{
+      background: blue;
+    }
+  </style>
   </head>
   <body>
-    <table>
-      <tbody>
-        <tr>
-          <td>Jose Vazquez</td>
-        </tr>
-        <tr>
-          <td>Telefono</td>
-          <td>664646</td>
-        </tr>
-        <tr>
-          <td>Fecha</td>
-          <td>02-12-2020</td>
-        </tr>
-        <tr>
-          <td>&nbsp;</td>
-        </tr>
-        <tr>
-          <td>Juan Jose</td>
-        </tr>
-        <tr>
-          <td>Telefono</td>
-          <td>664646</td>
-        </tr>
-        <tr>
-          <td>Fecha</td>
-          <td>02-12-2020</td>
-        </tr>
-      </tbody>
-    </table>
+      <div class="container-msg">
+        <p class="title-msg">Titulo de mensaje 1</p>
+        <p class="title-content-msg">Mensaje 1</p>
+      </div>
+
+      <div class="container-msg">
+        <p class="title-msg">Titulo de mensaje 2</p>
+        <p class="title-content-msg">Mensaje 2</p>
+      </div>
+
+      <div class="container-msg">
+        <p class="title-msg">Titulo de mensaje 3</p>
+        <p class="title-content-msg">Mensaje 3</p>
+      </div>
   </body>
 </html>
