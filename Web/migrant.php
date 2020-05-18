be711c3aacdad81d0d93b9975591f49af580951b
CotaVilla <42920256+CotaVilla@users.noreply.github.com>
2020-05-18 02:54:50 -0700

    Consulta migrantes medio
    Back

--- Web/migrant.php
+++ Web/migrant.php
@@ -19,28 +19,26 @@
         <div class="select">
           <label for="order">Ordenar por:</label>
           <select class="" name="order">
-            <option value=""></option>
-            <option value="">Fecha de llegada asc</option>
-            <option value="">Fecha de llegada desc</option>
-            <option value="">Nombre asc</option>
-            <option value="">Nombre desc</option>
-            <option value="">Cita consulado asc</option>
-            <option value="">Cita consulado desc</option>
+            <option >Fecha de llegada asc</option>
+            <option >Fecha de llegada desc</option>
+            <option >Nombre asc</option>
+            <option selected>Nombre desc</option>
+            <option >Cita consulado asc</option>
+            <option >Cita consulado desc</option>
           </select>
         </div>
         <div class="select">
           <label for="show">Mostrar:</label>
           <select class="" name="show">
-            <option value=""></option>
-            <option value="">Último mes</option>
-            <option value="">Últimos 3 meses</option>
-            <option value="">Últimos 6 meses</option>
-            <option value="">Todos</option>
+            <option value="mes">Último mes</option>
+            <option value="meses3">Últimos 3 meses</option>
+            <option value="meses6">Últimos 6 meses</option>
+            <option value="Todos" selected>Todos</option>
           </select>
         </div>
       </div>
       <div class="container-table" style="overflow-x:auto;">
-        <table id="table-migrants">
+        <table id="table-migrants" class="tablesorter">
             <thead>
               <tr>
                 <td style="display: none;">ID</td>
@@ -54,31 +52,10 @@
                 <th></th>
               </tr>
             </thead>
-            <tbody id="tasks-migrants">
-              <tr class="tr">
-                <td style="display: none;">1</td>
-                <td class="min-width"><input class="min-width-txt" type="text" name="" value="05-12-2020"></td>
-                <td><input type="text" name="" value="Juan Jose Ricarda"></td>
-                <td class="min-width"><input class="min-width-txt" type="text" name="" value="05-12-2020"></td>
-                <td class="min-width"><input class="min-width-txt" type="text" name="" value="09:00 am"></td>
-                <td class="min-width"><input class="min-width-txt" type="text" name="" value="10:00 am"></td>
-                <td><input type="text" name="" value="Hondureño"></td>
-                <td><input type="text" name="" value="664-123-1234"></td>
-                <td class="container-menu menu">
-                  <div class="options menu-1">
-                    <div class="option">
-                      <p class="text-menu edit-1">Editar</p>
-                    </div>
-                    <div class="option">
-                      <p class="text-menu">Eliminar</p>
-                    </div>
-                  </div>
-                </td>
-              </tr>
-            </tbody> <!--Donde se hace la magia con json-->
+            <tbody id="tasks-migrants"></tbody> <!--Donde se hace la magia con json-->
           </table>
       </div>
     </div>
   </body>
-  <script src="js/app.js"></script> <!--Manda a llamar al json-->
+  <script src="js/consult-migrant.js"></script> <!--Manda a llamar al json-->
 </html>
