541a3e8ff3ab2914f3f11b069270ee6c420efd94
jrg131298 <42877387+jrg131298@users.noreply.github.com>
2020-05-30 10:54:42 -0700

    Backend
    Wea filtros

--- Web/migrant.php
+++ Web/migrant.php
@@ -22,7 +22,7 @@
             <option value="1">Fecha de llegada asc</option>
             <option value="2">Fecha de llegada desc</option>
             <option value="3">Nombre asc</option>
-            <option value="4" selected>Nombre desc</option>
+            <option value="4">Nombre desc</option>
        <!-- <option value="5">Cita consulado asc</option>
             <option value="6">Cita consulado desc</option> -->
           </select>
@@ -33,7 +33,7 @@
             <option value="mes">Último mes</option>
             <option value="meses3">Últimos 3 meses</option>
             <option value="meses6">Últimos 6 meses</option>
-            <option value="Todos" selected>Todos</option>
+            <option value="Todos">Todos</option>
           </select>
         </div>
       </div>
