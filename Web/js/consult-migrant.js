3c8ea773bf61489548226b10675679251d3bf1d5
jrg131298 <42877387+jrg131298@users.noreply.github.com>
2020-05-30 09:54:56 -0700

    Backend
    Weas ordenamiento

--- Web/js/consult-migrant.js
+++ Web/js/consult-migrant.js
@@ -33,6 +33,71 @@ $(document).ready(function() {
     });
   });
 
+
+
+    $("#mySelect").change(function() {
+        var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+        var typeSelect = document.getElementById("mySelect").value;
+        table = document.getElementById("table-migrants");
+        switching = true;
+        if(typeSelect == "1" || typeSelect == "3")
+        {
+            //Set the sorting direction to ascending:
+            dir = "asc"; 
+        }
+        else if(typeSelect == "2" || typeSelect == "4")
+        {
+            dir = "desc";
+        }
+        
+        /*Make a loop that will continue until
+        no switching has been done:*/
+        while (switching) 
+        {
+            //start by saying: no switching is done:
+            switching = false;
+            rows = table.rows;
+            /*Loop through all table rows (except the
+            first, which contains table headers):*/
+            for (i = 1; i < (rows.length - 1); i++) {
+                //start by saying there should be no switching:
+                shouldSwitch = false;
+                /*Get the two elements you want to compare,
+                one from current row and one from the next:*/
+                x = rows[i].getElementsByTagName("TD")[1];
+                y = rows[i + 1].getElementsByTagName("TD")[1];
+                /*check if the two rows should switch place,
+                based on the direction, asc or desc:*/
+                if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch= true;
+                    break;
+                }
+                } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+                }
+            }
+            if (shouldSwitch) {
+                /*If a switch has been marked, make the switch
+                and mark that a switch has been done:*/
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+                //Each time a switch is done, increase this count by 1:
+                switchcount ++;      
+            } 
+            else {
+                /*If no switching has been done AND the direction is "asc",
+                set the direction to "desc" and run the while loop again.*/
+            }
+        }
+    });
+
+
     /*Usado en migrant.php*/
     /*Funcion para imprimir las filas de los migrantes existentes*/
     function obtener() {
--- Web/migrant.php
+++ Web/migrant.php
@@ -18,18 +18,18 @@
       <div class="container-select">
         <div class="select">
           <label for="order">Ordenar por:</label>
-          <select class="" name="order">
-            <option >Fecha de llegada asc</option>
-            <option >Fecha de llegada desc</option>
-            <option >Nombre asc</option>
-            <option selected>Nombre desc</option>
-            <option >Cita consulado asc</option>
-            <option >Cita consulado desc</option>
+          <select class="" name="order" id="mySelect">
+            <option value="1">Fecha de llegada asc</option>
+            <option value="2">Fecha de llegada desc</option>
+            <option value="3">Nombre asc</option>
+            <option value="4" selected>Nombre desc</option>
+       <!-- <option value="5">Cita consulado asc</option>
+            <option value="6">Cita consulado desc</option> -->
           </select>
         </div>
         <div class="select">
           <label for="show">Mostrar:</label>
-          <select class="" name="show">
+          <select class="" name="show" id="mySelect2">
             <option value="mes">Último mes</option>
             <option value="meses3">Últimos 3 meses</option>
             <option value="meses6">Últimos 6 meses</option>
