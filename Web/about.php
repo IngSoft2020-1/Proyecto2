5e4ea2e7090c80b94a78ca3e7e13363d01ab966f
AbnerPerales19 <42877917+AbnerPerales19@users.noreply.github.com>
2020-07-08 21:27:21 -0700

    Pequeño ajuste en Acerca de

--- Web/about.php
+++ Web/about.php
@@ -9,40 +9,17 @@
     <meta charset="utf-8">
     <!-- JQUERY -->
     <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
-    <script src="js/migrant.js"></script>
-    <!-- <link rel="stylesheet" href="css/edit.css"> -->
-    <link rel="stylesheet" href="css/migrant.css">
-    <link rel="stylesheet" href="css/new.css">
-    <!-- ESTA LIBRERIA LA CREO: EDUARDO BLANCO
-        PARA PODER MANDAR A LLAMAR UN MENSAJE
-        DE VALIDACION -->
-    <script src="js/msg-alert.js"></script>
-    <link rel="stylesheet" href="css/msg-alert.css">
-    <!-- FIN DE LA LIBRERIA -->
-
-    <!-- LIBRERIA PARA LOS POPUPS -->
-    <script src="js/popup.js"></script>
-    <link rel="stylesheet" href="css/popup.css">
-    <!-- FIN DE LA LIBRERIA -->
-
-    <!-- LIBRERIAS PARA DATEPICKER -->
-    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link rel="stylesheet" href="/resources/demos/style.css">
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <!-- MASCARA -->
-    <script type="text/javascript" src="js/mask/src/jquery.mask.js"></script>
-    <title></title>
-
+    <link rel="stylesheet" href="css/about.css">
     <!-- SCRIPTS -->
     <script type="text/javascript" src="js/new-migrant.js"></script>
     <link rel="stylesheet" href="css/edit.css">
   </head>
   <body>
-    <div class="container" style="justify-content: center !important;">
+    <div class="centrado1">
       <br>
-      <img  src="img/itt.png">
-      <img src="img/tecnm.jpg">
+      <img src="img/TECNM_PNG.png" class="img1">
+      <img  src="img/ITT_PNG.png" class="img2">
+    </div>
       <div >
         <p style="color: #FFFFFF">Proyecto desarrollado en colaboración con el Tecnológico Nacional de México/Instituto Tecnológico de Tijuana. Ingeniería de Software 2020-1. </p>
         <br><br>
@@ -66,7 +43,6 @@
           RAMIREZ GARCIA JESUS
         </ul>
       </div>
-    </div>
 
 
   </body>
