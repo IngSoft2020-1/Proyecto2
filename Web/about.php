350442ad79cbabc6bdb0d2c158d2ab2183ce7f47
AbnerPerales19 <42877917+AbnerPerales19@users.noreply.github.com>
2020-08-25 14:47:24 -0700

    About

--- Web/about.php
+++ Web/about.php
@@ -23,34 +23,43 @@
 
     <h1 style="color: #FFFFFF">¿Qui&eacute;nes somos?</h1>
 
-    <div class="container">
-        <FONT FACE="impact" SIZE=4>
-        <p style="color: #EC6D4A; text-align: justify;">Un grupo de estudiantes del Instituto Tecnológico de Tijuana del Tecnológico Nacional de México. Actualmente cursando la carrera de Ingeniería en Sistemas Computacionales en el semestre Enero-Junio (2020-1). </p>
-        <br>
-        <p style="color: #EC6D4A">Integrantes:</p>
-        <ul style="color: #EC6D4A">
-          BLANCO MELENDEZ MANUEL EDUARDO
-        </ul>
-        <ul style="color: #EC6D4A">
-          CARLOS ARMANDO CARLON GERARDO
-        </ul>
-        <ul style="color: #EC6D4A">
-          COTA VILLA EDY
-        </ul>
-        <ul style="color: #EC6D4A">
-          MORGADO JACOME EDUARDO
-        </ul>
-        <ul style="color: #EC6D4A">
-          PERALES NIEBLA ABNER JESUS
-        </ul>
-        <ul style="color: #EC6D4A">
-          RAMIREZ GARCIA JESUS
-        </ul>
-      </FONT>
+    <div class="container-table">
+      <table>
+        <thead>
+          <tr>
+            <FONT FACE="impact" SIZE=4>
+            <p style="color: #EC6D4A; text-align: left;"> Un grupo de estudiantes del Instituto Tecnológico de Tijuana del Tecnológico Nacional de México. Actualmente cursando la carrera de Ingeniería en Sistemas Computacionales en el semestre Enero-Junio (2020-1). </p>
+            <br>
+            <p style="color: #EC6D4A">Integrantes:</p>
+            <ul style="color: #EC6D4A">
+              BLANCO MELENDEZ MANUEL EDUARDO
+            </ul>
+            <ul style="color: #EC6D4A">
+              CARLOS ARMANDO CARLON GERARDO
+            </ul>
+            <ul style="color: #EC6D4A">
+              COTA VILLA EDY
+            </ul>
+            <ul style="color: #EC6D4A">
+              MORGADO JACOME EDUARDO
+            </ul>
+            <ul style="color: #EC6D4A">
+              PERALES NIEBLA ABNER JESUS
+            </ul>
+            <ul style="color: #EC6D4A">
+              RAMIREZ GARCIA JESUS
+            </ul>
+          </FONT>
+        </tr>
+      </thead>
+      <thead>
+        <tr>
+          <br><br>
+          <p style="color: #00FF80;"> Proyecto desarrollado en colaboración con el Instituto Tecnológico de Tijuana del Tecnológico Nacional de México, para el curso de Ingeniería en Software.</p>
+        </tr>
+      </thead>
+    </table>
     </div>
-
-    <br><br><br>
-    <p style="color: #00FF80;"> Proyecto desarrollado en colaboración con el Instituto Tecnológico de Tijuana del Tecnológico Nacional de México, para el curso de Ingeniería en Software.</p>
 
 
 
