3975a731c4b216c6c12b6e9b67ad65914e6f337c
AbnerPerales19 <42877917+AbnerPerales19@users.noreply.github.com>
2020-08-25 14:26:00 -0700

    About

--- Web/about.php
+++ Web/about.php
@@ -15,11 +15,16 @@
     <link rel="stylesheet" href="css/edit.css">
   </head>
   <body>
+    <div class="container" style="color: white; text-align:center !important;">
+      <br>
+      <img src="img/TECNM_PNG.png" class="img1" style="width: 350px !important; height: 200px !important;">
+      <img  src="img/ITT_PNG.png" class="img2" style="width: 200px !important; height: 200px !important;">
+    </div>
 
     <h1 style="color: #FFFFFF">¿Qui&eacute;nes somos?</h1>
 
     <div class="container">
-        <FONT FACE="impact" SIZE=6>
+        <FONT FACE="impact" SIZE=4>
         <p style="color: #EC6D4A; text-align: justify;">Un grupo de estudiantes del Instituto Tecnológico de Tijuana del Tecnológico Nacional de México. Actualmente cursando la carrera de Ingeniería en Sistemas Computacionales en el semestre Enero-Junio (2020-1). </p>
         <br>
         <p style="color: #EC6D4A">Integrantes:</p>
@@ -47,11 +52,7 @@
     <br><br><br>
     <p style="color: #00FF80;"> Proyecto desarrollado en colaboración con el Instituto Tecnológico de Tijuana del Tecnológico Nacional de México, para el curso de Ingeniería en Software.</p>
 
-    <div class="container" style="color: white; text-align:center !important;">
-      <br>
-      <img src="img/TECNM_PNG.png" class="img1" >
-      <img  src="img/ITT_PNG.png" class="img2">
-    </div>
+
 
 
 
